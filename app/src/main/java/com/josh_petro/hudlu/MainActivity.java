32a1acf9eaeee4b2d4cf1676144f5f3d6514c5b0
Josh Petro <josh.petro@hudl.com>
2016-03-10 12:26:22 +1100

    Updated Utilities file

--- app/src/main/java/com/josh_petro/hudlu/MainActivity.java
+++ app/src/main/java/com/josh_petro/hudlu/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+
         if (id == R.id.action_settings) {
             android.util.Log.d("HudlU", "Settings menu item clicked.");
             return true;
