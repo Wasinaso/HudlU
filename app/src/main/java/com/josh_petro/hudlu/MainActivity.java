2949ec2641ea0a30eed47b5d5f69df74c31d5228
Josh Petro <josh.petro@hudl.com>
2016-03-28 13:46:39 +1100

    Added Recycler view

--- app/src/main/java/com/josh_petro/hudlu/MainActivity.java
+++ app/src/main/java/com/josh_petro/hudlu/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import org.apache.commons.logging.Log;
+
 
 public class MainActivity extends ActionBarActivity {
 
--- app/src/main/java/com/wasinaso/joshpetro/application/MainActivity.java
+++ app/src/main/java/com/wasinaso/joshpetro/application/MainActivity.java
@@ -1,13 +1,18 @@
 package com.wasinaso.joshpetro.application;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.josh_petro.hudlu.R;
 
 public class MainActivity extends ActionBarActivity {
+    //private RecyclerView mRecyclerView;
+    //private RecyclerView.Adapter mAdapter
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,4 +41,28 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public class MyActivity extends Activity {
+        private RecyclerView mRecyclerView;
+        private RecyclerView.Adapter mAdapter;
+        private RecyclerView.LayoutManager mLayoutManager;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            mRecyclerView.setHasFixedSize(true);
+
+            // use a linear layout manager
+            mLayoutManager = new LinearLayoutManager(this);
+            mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        }
+    }
+
 }
