cf776aeccf0dfa977e344f932bacba207e30ebf9
Josh Petro <josh.petro@hudl.com>
2016-03-29 16:56:43 +1100

    Moved Adaptor (no idea where to put it honestly) and then added the card view.

--- app/src/main/java/com/josh_petro/hudlu/MainActivity.java
+++ app/src/main/java/com/josh_petro/hudlu/MainActivity.java
@@ -1,7 +1,10 @@
 package com.josh_petro.hudlu;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -38,5 +41,31 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    public class MyActivity extends Activity {
+        private RecyclerView mRecyclerView;
+        private RecyclerView.Adapter mAdapter;
+        private RecyclerView.LayoutManager mLayoutManager;
 
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            mRecyclerView.setHasFixedSize(true);
+
+            // use a linear layout manager
+            mLayoutManager = new LinearLayoutManager(this);
+            mRecyclerView.setLayoutManager(mLayoutManager);
+
+            // specify an adapter (see also next example)
+            //mAdapter = new MyAdapter(myDataset);
+            mRecyclerView.setAdapter(mAdapter);
+
+
+
+        }
+    }
 }
